6fd10f73e5a1017ffc88d7a0f0726eeffc523b88
German Sayago <german.sayago@gmail.com>
2020-03-18 22:55:57 -0300

    update image hero

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -3,15 +3,10 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-10 col-lg-8">
-          <h1
-            class="display-1"
-            data-aos="zoom-in"
-            data-aos-delay="50"
-            data-aos-once="true"
-          >
+          <h1 class="display-1">
             Escalemos tu negocio a su máximo potencial
           </h1>
-          <h4 data-aos="fade-up" data-aos-delay="400" data-aos-once="true">
+          <h4>
             Construimos estrategias digitales de alto valor, claves para el
             futuro de tu negocio en internet.
           </h4>
@@ -19,9 +14,6 @@
             v-scroll-to="{ el: '#estrategy', offset: -50 }"
             href="#"
             class="btn btn-outline-light text-uppercase"
-            data-aos="fade-up"
-            data-aos-delay="800"
-            data-aos-once="true"
             >¡Comencemos!</a
           >
         </div>
